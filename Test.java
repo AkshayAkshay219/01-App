3dbc5b504e8923fe4a4cd228cc273f4dcc28d294
Akshay <akshaygowdauibps@gmail.com>
2025-10-07 12:27:40 +0530

    task-100 completed after task-101

--- Test.java
+++ Test.java
@@ -5,11 +5,19 @@ int b=20;
      System.out.println(b);   
 }
 
+<<<<<<< Updated upstream
 //task-101 -> on priority basis
 
-public void m2()
+public void m2(){
 double d1=567.32d;
 float f1=456.78;
+} 
+
+//task-100
+public void m1(){
+long l=15352;
+//logic-in progress
+//logic completed after task 101
 
 }
 
