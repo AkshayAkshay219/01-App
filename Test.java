537992debb5ab42c5d8f865c08d77d82ea4c9f77
AkshayAkshay219 <akshaygowdauibps@gmail.com>
2025-10-07 12:20:16 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,28 +1,9 @@
 class Akshay{
-
 int b=20;
+  
   public static void main(String[] args){
-   
-     System.out.println(b);
-    
+     System.out.println(b);   
 }
-<<<<<<< Updated upstream
-
-//starting task-101 as priority
-
-public void m2(){
-System.out.println("Hello in m2()");
-System.out.println(b);
-}
-}
-=======
-//task-100 in progress
-
-public void m1(){
-int a=10;
-int b=20;
-System.out.println("Hello m1()");
-//logic
 
 }
->>>>>>> Stashed changes
+
